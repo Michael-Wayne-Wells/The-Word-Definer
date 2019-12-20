b284b8d2b971d5021c04b70631ffecbdc0116887
Michael Wells <mwells1286@gmail.com>
2019-12-20 08:28:34 -0800

    add test for adding new words to word class.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,4 +7,13 @@ describe(Word) do
       expect(Word.all).to(eq([]))
     end
   end
+  describe('#save') do
+    it('add new word to class') do
+      word = Word.new({:name => "steve", :id => nil})
+      word.save
+      word2 = Word.new({:name => "taco", :id => nil})
+      word2.save
+      expect(Word.all).to(eq([word1, word2]))
+    end
+  end
 end

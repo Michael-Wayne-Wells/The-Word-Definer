8e9b94f4184ac3dc1c5691996d56fe3729c584db
Michael Wells <mwells1286@gmail.com>
2019-12-20 08:21:07 -0800

    add test to return empty array when no words exist

---
+++ spec/definition_spec.rb
@@ -0,0 +1,11 @@
+# require('rspec')
+# require('definition')
+#
+# describe(Definition) do
+#   describe('#new') do
+#     it('test') do
+#       test = Definition.new()
+#       expect(test.new()).to(eq('test'))
+#     end
+#   end
+# end

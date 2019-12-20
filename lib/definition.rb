534ae005d263d0f91fae0692b6324e5f5de24342
Michael Wells <mwells1286@gmail.com>
2019-12-20 09:49:33 -0800

    add definition page with methods add new definitions for words. Passes test

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,7 +1,30 @@
 class Definition
-  def initialize
-  end
-  def new()
+  attr_reader :id
+  attr_accessor :text
 
+  @@defintions = {}
+  @@rows = 0
+
+  def initialize(attributes)
+    @text = attributes.fetch(:text)
+    @id = attributes.fetch(:id) || @@rows += 1
   end
+
+  def save
+    @@defintions[self.id] = Definition.new({ :text => self.text, :id => self.id})
+  end
+
+  def self.all
+    @@defintions.values()
+  end
+
+  def==(to_compare)
+    self.text.eql?(to_compare.text())
+  end
+
+  def self.clear
+    @@defintions = {}
+    @@rows = 0
+  end
+
 end

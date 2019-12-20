e1b67038e3ee94f6d7f6c9cba760e5876c58be1f
Michael Wells <mwells1286@gmail.com>
2019-12-20 08:44:34 -0800

    Add test for method to clear word list.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('word')
 
 describe(Word) do
+  before(:each) do
+    Word.clear
+  end
   describe('.all') do
     it('returns empty array when no words') do
       expect(Word.all).to(eq([]))
@@ -14,6 +17,16 @@ describe(Word) do
       word2 = Word.new({:name => "taco", :id => nil})
       word2.save
       expect(Word.all).to(eq([word, word2]))
+    end
+  end
+  describe('.clear') do
+    it('clears word list') do
+      word = Word.new({:name => "steve", :id => nil})
+      word.save
+      word2 = Word.new({:name => "taco", :id => nil})
+      word2.save
+      Word.clear
+      expect(Word.all).to(eq([]))
     end
   end
 end

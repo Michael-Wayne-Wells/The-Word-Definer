654433fc0830ade83b30c940cb51caf789d3803b
Michael Wells <mwells1286@gmail.com>
2019-12-20 09:56:38 -0800

    add test to find a definition by id

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -24,4 +24,13 @@ describe(Definition) do
       expect(definition.text).to(eq('A cool guy'))
     end
   end
+  describe('.find') do
+    it('finds a definition on the list by the id') do
+      definition = Definition.new({:text => "A cool guy", :word_id => @word.id, :id => nil})
+      definition.save
+      definition1 = Definition.new({:text => "to do something poorly", :word_id => @word.id, :id => nil})
+      definition1.save
+      expect(Definition.find(definition2.id)).to(eq(definition2))
+    end
+  end
 end

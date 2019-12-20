24c9da79f17d5f4be45c4768fc93c6abee3392e4
Michael Wells <mwells1286@gmail.com>
2019-12-20 09:54:24 -0800

    add test to check if inputed definition is saved. Passes without changes to method.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,13 +8,20 @@ describe(Definition) do
     Definition.clear
     @word = Word.new({:name => "doug", :id => nil})
   end
-  describe('#all') do
-    it('adds and returns definitions of words') do
+  describe('.all') do
+    it('returns definitions of words') do
       definition = Definition.new({:text => "A cool guy", :word_id => @word.id, :id => nil})
       definition.save
       definition1 = Definition.new({:text => "to do something poorly", :word_id => @word.id, :id => nil})
       definition1.save
       expect(Definition.all).to(eq([definition, definition1]))
+    end
+  end
+  describe('#save') do
+    it('saves new definition for word and assigns id') do
+      definition = Definition.new({:text => "A cool guy", :word_id => @word.id, :id => nil})
+      definition.save
+      expect(definition.text).to(eq('A cool guy'))
     end
   end
 end

516fd8cd7b5c1acf07c3af9794c7e24c7ab2ee35
Michael Wells <mwells1286@gmail.com>
2019-12-20 14:50:11 -0800

    Add integration test for adding a definition

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -12,3 +12,13 @@ describe('add a a new word', {:type => :feature}) do
     expect(page).to have_content('dialectics')
   end
 end
+describe('add a a new definition', {:type => :feature}) do
+  it('adds word and returns to word list') do
+    visit('/words')
+    click_on('dialectics')
+    fill_in("new_def", :with => "fighting words")
+    click_on('Submit')
+    expect(page).to have_content('fighting words')
+
+  end
+end
